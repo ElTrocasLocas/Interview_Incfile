30be528893d8e2fb3930edc58b6a27d019b1a2a0
ElTrocasLocas <141537467+ElTrocasLocas@users.noreply.github.com>
2023-09-05 21:23:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 const puppeteer = require('puppeteer');
 require('dotenv').config();
 try {
-    
-
-(async() => {
+    (async() => {
     const browser = await puppeteer.launch({
         headless: false
-        //defaultViewport: null //(I cant use this feature because I have puppeteer v 1.0.0 and I don't know how to change it yet)
     });
-
     const usuario = process.env.USUARIO;
     const contrasena = process.env.CONTRASENA;
-
+//Copiar datos de trello
     const page = await browser.newPage();
     await page.goto('https://trello.com/b/QvHVksDa/personal-work-goals');
     const texto1 = await page.$eval('#board > div:nth-child(1) > div > div.list-cards.u-fancy-scrollbar.js-list-cards.has-margin-bottom > a:nth-child(1) > div.list-card-details.js-card-details > span', element => element.textContent);
@@ -19,8 +15,11 @@ try {
     const texto3 = await page.$eval('#board > div:nth-child(1) > div > div.list-cards.u-fancy-scrollbar.js-list-cards.has-margin-bottom > a:nth-child(3) > div.list-card-details.js-card-details > span', element => element.textContent);
     const texto4 = await page.$eval('#board > div:nth-child(1) > div > div.list-cards.u-fancy-scrollbar.js-list-cards.has-margin-bottom > a:nth-child(4) > div.list-card-details.js-card-details > span', element => element.textContent);
     const texto5 = await page.$eval('#board > div:nth-child(2) > div > div.list-cards.u-fancy-scrollbar.js-list-cards.has-margin-bottom > a:nth-child(1) > div.list-card-details.js-card-details > span', element => element.textContent);
+    const textos = [texto1, texto2, texto3, texto4, texto5];
     await page.waitForTimeout(3000);
-     await page.goto('https://todoist.com/auth/login');
+
+//Iniciar sesion en todoist
+    await page.goto('https://todoist.com/auth/login');
     await page.waitForTimeout(2000);
     await page.type('#element-0',(`${usuario}`));
     await page.waitForTimeout(1000);
@@ -28,33 +27,17 @@ try {
     await page.waitForTimeout(1000);
     await page.keyboard.press('Enter')
     await page.waitForTimeout(5000); 
-    await page.keyboard.press('q');
-    await page.type('.ProseMirror',texto1);
-    await page.waitForTimeout(2000); 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('q');
-    await page.type('.ProseMirror',texto2);
-    await page.waitForTimeout(2000); 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('q');
-    await page.type('.ProseMirror',texto3);
-    await page.waitForTimeout(2000); 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('q');
-    await page.type('.ProseMirror',texto4);
-    await page.waitForTimeout(2000); 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000);
-    await page.keyboard.press('q');
-    await page.type('.ProseMirror',texto5);
-    await page.waitForTimeout(2000); 
-    await page.keyboard.press('Enter');
-    await page.waitForTimeout(2000); 
+
+//pegar la informacion
+  for (const texto of textos) {
+  await page.keyboard.press('q');
+  await page.type('.ProseMirror', texto);
+  await page.waitForTimeout(2000);
+  await page.keyboard.press('Enter');
+  await page.waitForTimeout(2000);
+}
 }
 )();
 } catch (error) {
-    console.log('Error')
+    console.error('Error:', error.message);
 }
